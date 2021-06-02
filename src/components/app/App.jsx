c0224a4e88e0bdb21b8e64f8d6786cd94f2f16cb
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-06-02 14:12:36 -0700

    fix to page images sizing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import CharacterList from './CharacterList';
 import Header from './Header';
+import './app.css';
 
 export default function App() {
   return (
--- src/components/app/Loading.jsx
+++ src/components/app/Loading.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable indent */
 /* eslint-disable max-len */
 import React from 'react';
+import style from './app.css';
 
 export default function Loading() {
-    return <img src="https://cdn.dribbble.com/users/841405/screenshots/2541549/animaion-bb.gif" />;
+    return <img className={style.loading} src="https://cdn.dribbble.com/users/841405/screenshots/2541549/animaion-bb.gif" />;
 }
